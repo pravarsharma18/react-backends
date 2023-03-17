fb63cefd37cdc20c1bb5e0daf2f26ffda79d8aa5
pravar <pravar.sharma@trootech.com>
2023-03-17 17:58:13 +0530

    applyed loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import ProductList from "./components/ProductList";
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 
 interface User {
   id: number;
@@ -10,30 +10,32 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
-
-  // useEffect(() => {
-  //   axios
-  //     .get<User[]>("https://jsonplaceholder.typicode.com/susers")
-  //     .then((res) => setUsers(res.data))
-  //     .catch((err) => setError(err.message));
-  // }, []);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/users"
-        );
+    const controller = new AbortController(); // cancle or abort async operations, that takes long time to complete.
+    setIsLoading(true);
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
         setUsers(res.data);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-    };
-    fetchUsers();
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return; // otherwise it will show message on browser
+        setError(err.message);
+        setIsLoading(false);
+      });
+
+    return () => controller.abort();
   }, []);
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
